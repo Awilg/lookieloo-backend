ac0fed6536a02a658b6f6b4c9281d08271ff2bab
jgliwa <jgliwa12@gmail.com>
2021-01-27 22:08:08 -0800

    formatting

--- lookieloo-service-loo/src/com/lookieloo/services/loo/data/LooRepository.kt
+++ lookieloo-service-loo/src/com/lookieloo/services/loo/data/LooRepository.kt
@@ -7,29 +7,35 @@ import org.litote.kmongo.coroutine.CoroutineCollection
 
 class LooRepository(private val collection: CoroutineCollection<Loo>) : BaseDaoAsync<Loo>(collection) {
 
-	//47.6062° N, 122.3321° W
-	// the distances are in meters btw
-	suspend fun findByLocation(latitude : Double, longitude: Double, minDistance : Double, maxDistance : Double, filterParams: List<String>?): List<Loo> {
-		var filter = filterParams?.let { filterList ->
-			"attributes: { \$all: [${filterList.joinToString(", ", "\"", "\"")}] }"
-		}
-		filter = filter ?: ""
+    //47.6062° N, 122.3321° W
+    // the distances are in meters btw
+    suspend fun findByLocation(
+        latitude: Double,
+        longitude: Double,
+        minDistance: Double,
+        maxDistance: Double,
+        filterParams: List<String>?
+    ): List<Loo> {
+        var filter = filterParams?.let { filterList ->
+            "attributes: { \$all: [${filterList.joinToString(", ", "\"", "\"")}] }"
+        }
+        filter = filter ?: ""
 
-		val results = collection.find(
-			"{ " +
-					"location: {" +
-					"${MongoOperator.nearSphere}:{" +
-					"${MongoOperator.geometry}:{" +
-					"type: \"Point\"," +
-					"coordinates: [${longitude}, ${latitude}], }," +
-					"\$maxDistance: $maxDistance," +
-					"\$minDistance: $minDistance," +
-					"}," +
-					"}," +
-					"$filter" +
-					"}"
-		)
+        val results = collection.find(
+            "{ " +
+                    "location: {" +
+                    "${MongoOperator.nearSphere}:{" +
+                    "${MongoOperator.geometry}:{" +
+                    "type: \"Point\"," +
+                    "coordinates: [${longitude}, ${latitude}], }," +
+                    "\$maxDistance: $maxDistance," +
+                    "\$minDistance: $minDistance," +
+                    "}," +
+                    "}," +
+                    "$filter" +
+                    "}"
+        )
 
-		return results.toList()
-	}
+        return results.toList()
+    }
 }
