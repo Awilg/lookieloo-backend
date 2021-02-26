5f600927fa47350c243a2b3aac82284f383fcdf0
James Gliwa <james@Jamess-MBP.localdomain>
2021-02-26 11:26:34 -0800

    dont create new ECR

--- ecs-devops-sandbox-cdk/ecs_devops_sandbox_cdk/ecs_devops_sandbox_cdk_stack.py
+++ ecs-devops-sandbox-cdk/ecs_devops_sandbox_cdk/ecs_devops_sandbox_cdk_stack.py
@@ -1,15 +1,10 @@
 """AWS CDK module to create ECS infrastructure"""
-from aws_cdk import (core, aws_ecs as ecs, aws_ecr as ecr, aws_ec2 as ec2, aws_iam as iam)
+from aws_cdk import (core, aws_ecs as ecs, aws_ecr as ecr, aws_ec2 as ec2, aws_iam as iam, aws_ecs_patterns as ecs_patterns)
 
 class EcsDevopsSandboxCdkStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-
-        # Create the ECR Repository
-        ecr_repository = ecr.Repository(self,
-                                        "ecs-devops-sandbox-repository",
-                                        repository_name="ecs-devops-sandbox-repository")
 
         # Create the ECS Cluster (and VPC)
         vpc = ec2.Vpc(self,
@@ -47,7 +42,7 @@ class EcsDevopsSandboxCdkStack(core.Stack):
         )
 
         # Create the ECS Service
-        service = ecs.FargateService(self,
+        service = ecs_patterns.ApplicationLoadBalancedFargateService(self,
                                      "ecs-devops-sandbox-service",
                                      cluster=cluster,
                                      task_definition=task_definition,
